6cd3d83394e559679f86792623efdba87460b0ab
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-08-11 22:39:51 -0400

    kinda finished the rest all at once sorry

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,48 +8,56 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
 function generatePassword() {  // Your code goes here.
 
-  // Declare variables for all possible password characters, grouped by criteria.
+  // Declare variables to store the character set of each criteria.
   var lowerCase = "abcdefghijklmnopqrstuvwxyz"
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var numeric = "1234567890"
-  var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "
+  var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~ "
 
-  // Turn them into arrays.
-  lowerArray = lowerCase.split("")
-  upperArray = lowerCase.split("")
-  numericArray = lowerCase.split("")
-  specialArray = lowerCase.split("")
-  
+  // Turn the variables into arrays and create an additional array to hold their combined values.
+  var lowerCaseArray = lowerCase.split("")
+  var upperCaseArray = upperCase.split("")
+  var numericArray = numeric.split("")
+  var specialArray = special.split("")
+  var combinedArray = [];
 
-  // Request password criteria from user via dialogues.
-  // Prompt use to enter a password length. Declare it as a variable.
-  var includeLength = window.prompt("Choose a character length from 8 to 128.");
-  // If user cancels instead of entering a length end the function.
-  if (!includeLength) {
+  // Gather password criteria from user via dialogues. Store as vars.
+  var selectLength = parseInt(window.prompt("Choose a character length from 8 to 128."));
+  // Close dialogues if user chooses to cancel.
+  if (!selectLength) {
     return;
-  // If password length is valid user then confirm or !confirm criteria.
-  } else if (includeLength >= 8 && includeLength <= 128) {
-    var includeLowerCase = window.confirm("Include uppercase in your password?");
-    var includeUpperCase = window.confirm("Include lowercase in your password?");
-    var includeNumeric = window.confirm("Include numbers in your password?");
-    var includeSpecial = window.confirm("Include special characters in your password?");
-  // If password length is not valid the dialogue reminds them and the function calls itself to start over.
-  } else {
-    window.alert("The character length MUST be a number between 8 and 128");
+    // If password length invalid alert user and restart dialogues.
+  } if (selectLength < 8 || selectLength > 128) {
+    window.alert("The character length must be a number between 8 and 128. Try again!");
     generatePassword();
+    // If password length valid gather remaining criteria and combined the selected character sets into a single array.
+  } if (window.confirm("Include lowercase characters?")) {
+    combinedArray = combinedArray.concat(lowerCaseArray);
+  } if (window.confirm("Include uppercase characters?")) {
+    combinedArray = combinedArray.concat(upperCaseArray);
+  } if (window.confirm("Include numbers")) {
+    combinedArray = combinedArray.concat(numericArray)
+  } if (window.confirm("Include special characters?")) {
+    combinedArray = combinedArray.concat(specialArray)
+     // Return to start over if user doesn't select any character set.
+  } if (combinedArray.length === 0) {
+    window.alert("The password needs at least one character set.");
+    return;
   }
 
-  // Express function to get random lower case characters.
-  function getRandomLowerCase() {
-    Math.random()
+  // Randomlly select elements from the combined array to generate the password.
+  var password = "";
+  for (var i = 0; i < selectLength; i++) {
+    var passwordIndex = Math.floor(Math.random() * combinedArray.length);
+    password = password + combinedArray[passwordIndex]
   }
 
-  
-
+  return password;
 }
 
 // Add event listener to generate button
